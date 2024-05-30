d2fa6181086ffea9673bf889ab830d6c2bdb7591
Julkzein <jules.delforge@epfl.ch>
2024-05-30 13:20:57 +0200

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import MainContent from "./components/Asset-page";
+import MainContent from "./components/BetElement";
 import Footer from "./components/Footer";
 import BetLogic from "./components/BetElement";
 
