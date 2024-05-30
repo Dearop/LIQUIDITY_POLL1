4b07bd26ce937103dc6957c10f330db2039aee66
Julkzein <jules.delforge@epfl.ch>
2024-05-30 13:10:24 +0200

    betElement

--- frontend/src/components/BetElement.js
+++ frontend/src/components/BetElement.js
@@ -1,58 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import Option from './Option';
-import {Bet}  from './Bet';
-import {BetPage} from './BetsPage';
-import {AssetPage}  from './Asset-page';
+import { Bet } from './Bet';
+import { BetPage } from './BetsPage';
+import { AssetPage } from './Asset-page';
 import MainContent from './MainContent';
 
-
-
 export default function BetLogic() {
+  const [showOwnPage, setShowOwnPage] = useState(false);
 
-    let showOwnPage = false;
-
-    
-
-    function handleClick() {
-        showOwnPage = true;
-        
-      }
-
-    return (
-        //<AssetPage {...{name : "zizi",
-                        //price: 12}} />
-        <div>
-            {showOwnPage ? <MainContent/> : <button onClick={handleClick}>
-                                                <Bet logo = "ta mere"
-                                                 title = "tamere"
-                                                 volume = "12"
-                                                 betUrl = ""
-                                                 coteOui = "1.5"
-                                                 coteNon = "1.2"
-            
-                                                />
-                                                
-                
-                                            </button>}
-            
-            
-        </div>
-        
-
-                        
-    );
+  function handleClick() {
+    setShowOwnPage(true);
   }
 
-
-
-
-
-
-
-
-
-
-
-
+  return (
+    <div>
+      {showOwnPage ? (
+        <MainContent />
+      ) : (
+        <button onClick={handleClick}>
+          <Bet
+            logo="ta mere"
+            title="tamere"
+            volume="12"
+            betUrl=""
+            coteOui="1.5"
+            coteNon="1.2"
+          />
+        </button>
+      )}
+    </div>
+  );
+}
